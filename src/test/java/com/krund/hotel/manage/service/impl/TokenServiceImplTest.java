7228aba79060f4d7b59b5700df277625768bb16e
qiangzi-johnny <1484104551@qq.com>
2018-09-13 17:13:16 +0800

    a

---
+++ src/test/java/com/krund/hotel/manage/service/impl/TokenServiceImplTest.java
@@ -0,0 +1,13 @@
+package com.krund.hotel.manage.service.impl;
+
+import com.krund.hotel.manage.service.BaseTest;
+import org.junit.Test;
+
+
+public class TokenServiceImplTest extends BaseTest {
+
+    @Test
+    public void test(){
+
+    }
+}
